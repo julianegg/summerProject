4c8262916c22e6af17d4a4d9f3a643644982c795
andreas riedmann <andreas@riedmann.rocks>
2024-05-16 13:55:04 +0200

    h2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>DEmo</h1>
-    <Header/>
+      <h1>DEmo</h1>
+      <h2>Super</h2>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
